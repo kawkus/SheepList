0f5cfcbad83f999875fdb54d7fcfa60b8229db1f
kawkus27 <kawkus273@gmail.com>
2018-02-18 20:38:05 +0100

    First Commit

---
+++ src/sheeplist/SheepList.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheeplist;
+
+import SheepListView.MainList;
+
+/**
+ *
+ * @author Użytkownik
+ */
+public class SheepList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainList mainListForm =  new MainList();
+        mainListForm.setVisible(true);
+        
+    }
+    
+}
