258a696cb4837a7bec9f5959b999824d277070fe
kawkus27 <kawkus273@gmail.com>
2018-02-19 21:40:28 +0100

    Add Sheep model class.

---
+++ src/SheepModel/Sheep.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SheepModel;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Użytkownik
+ */
+public class Sheep {
+    private String Id;
+    private Sheep Mother;
+    private LocalDate BirthDate;
+    private LocalDate LeaveDate;
+    private String ArrivePlaceInfo;
+    private char ArriveCode;
+    private char LeaveCode;
+    private String Sex;
+    private int BreedCode;
+    private String LeavePlaceInfo;
+    private String LeaveConveyor;
+    private String Note;
+    
+    
+    
+    
+   /* public void setDate()
+    {
+        BirthDate = LocalDate.of(2015, 11, 8);
+      
+    }*/
+    
+}
